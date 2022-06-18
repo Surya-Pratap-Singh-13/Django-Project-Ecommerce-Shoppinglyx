f4ca2fa7812cb5782e2f99d2c56e789df544efb5
Pratap <suryapratapsing8@gmail.com>
2022-06-18 21:53:40 +0530

    Commit shoppinglyx website

---
+++ shoppinglyx/app/models.py
@@ -0,0 +1,97 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator,MinLengthValidator
+
+STATE_CHOICE = (
+    ('Andaman & Nicobar Islands','Andaman & Nicobar Islands'),
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Chhattisgarh','Chhattisgarh'),
+    ('Dadra & Nagar Haveli','Dadra & Nagar Haveli'),
+    ('Baman and Diu','Baman and Diu'),
+    ('Goa','Goa'),
+    ('Gujarat','Gujarat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Jammu & Kashmir','Jammu & Kashmir'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('Kerala','Kerala'),
+    ('Lakshadweep','Lakshadweep'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalaya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisha','Odisha'),
+    ('Puducherry','Puducherry'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajasthan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttarakhand','Uttarakhand'),
+    ('Uttar Pradesh','Uttar Pradesh'),
+    ('West Bengal','West Bengal'),
+)
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.FloatField()
+    locality = models.FloatField()
+    city = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICE, max_length=50)
+
+def _str_(self):
+    return str(self.id)
+
+CATEGORY_CHOICE = (
+    ('M','Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Wear'),
+    ('BW','Bottom Wear'),
+)
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICE, max_length=2)
+    product_image = models.ImageField(upload_to='productimg')
+
+def _str_(self):
+    return str(self.id)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
+
+def _str_(self):
+    return str(self.id)
+
+STATUS_CHOICE = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+)
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50,choices=STATUS_CHOICE,default='Pending')
+
+
+
+# Create your models here.
